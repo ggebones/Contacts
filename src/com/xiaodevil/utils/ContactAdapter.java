7bb4c544d559a831141c38cce7f92a70ca425b1b
mcliu <1104513137@qq.com>
2015-01-19 21:57:53 +0800

    v0.1 not finish
    add the setting

--- src/com/xiaodevil/utils/ContactAdapter.java
+++ src/com/xiaodevil/utils/ContactAdapter.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.Random;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
@@ -16,7 +17,6 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.SectionIndexer;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.xiaodevil.contacts.R;
 import com.xiaodevil.models.User;
@@ -30,11 +30,15 @@ public class ContactAdapter extends BaseAdapter implements SectionIndexer,Filter
 	private ArrayList<User> mOriginalValues;
 	private ContactFilter mFilter;
 	private final Object mLock = new Object();
-	private Context context;
+	private Context context;	
+	private SharedPreferences preferences;
+	private final String TAG = "com.xiaodevil.utils.ContactAdapter";
+	
 	public ContactAdapter(Context context,int resourceid, List<User> list){
 		mObjects = list;
 		this.context = context;
 		this.resourceId = resourceid;
+		preferences = this.context.getSharedPreferences("settings", Context.MODE_PRIVATE);
 	}
 
 	@Override
@@ -55,10 +59,16 @@ public class ContactAdapter extends BaseAdapter implements SectionIndexer,Filter
 		avatar_bg.setBackgroundColor(android.graphics.Color.parseColor(color[index]));
 		
 		name.setText(user.getUserName());
-		
-		
-		//avatar.setText(user.getSortKey());
-		avatar.setText(user.getUserName().substring(0,1));
+		Log.e(TAG,preferences.getString("readType","letter"));
+		if(preferences.getString("readType","letter").equals("letter"))
+		{
+			
+			avatar.setText(user.getSortKey());
+		}
+		if(preferences.getString("readType","letter").equals("hanzi"))
+		{
+			avatar.setText(user.getUserName().substring(0,1));
+		}
 		return layout;
 	}
 
--- src/com/xiaodevil/views/SettingsActivity.java
+++ src/com/xiaodevil/views/SettingsActivity.java
@@ -1,20 +1,65 @@
 package com.xiaodevil.views;
 
-import com.xiaodevil.contacts.R;
-
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class SettingsActivity extends Activity{
+import com.xiaodevil.contacts.R;
+
+public class SettingsActivity extends ActionBarActivity{
+	
 	private SharedPreferences settings;
 	private SharedPreferences.Editor editor;
+	private Context useCount  = null;
+	
+	private RadioGroup radioGroup=null;
+	private RadioButton letterButton=null;
+	private RadioButton hanziButton=null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.settings);
-		settings = getSharedPreferences("setting",Context.MODE_PRIVATE);
+		useCount=this.getApplicationContext();
+		
+		
+		settings = useCount.getSharedPreferences("settings", MODE_PRIVATE);
 		editor = settings.edit();
+		
+		radioGroup=(RadioGroup)findViewById(R.id.radio_group);
+		letterButton=(RadioButton)findViewById(R.id.letter_button);
+		hanziButton=(RadioButton)findViewById(R.id.hanzi_button);
+		
+		if(settings.getString("readType", "hanzi").equals("hanzi"))
+			hanziButton.setChecked(true);
+		if(settings.getString("readType", "hanzi").equals("letter"))
+			letterButton.setChecked(true);
+		
+		radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				
+				if(checkedId==letterButton.getId())
+				{
+					//set sharepreference to leeter
+					editor.putString("readType", "letter");
+					System.out.println("letter");
+				}
+				if(checkedId==hanziButton.getId())
+				{
+					System.out.println("hanzi");
+					//set sharepreference to hanzi
+					editor.putString("readType", "hanzi");		
+				}
+				editor.commit();
+			}
+		});
+		
+	
 	}
+	
+	
 }
